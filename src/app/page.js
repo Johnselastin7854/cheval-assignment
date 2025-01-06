7d3aa18b5041f68cb63888fdf31135480016db11
selastinmathew <selastinmathew@gmail.com>
2025-01-06 15:36:37 +0400

    technlogySectionAdded

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import TechnologySection from "./components/TechnologySection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <TechnologySection />
     </>
   );
 }
